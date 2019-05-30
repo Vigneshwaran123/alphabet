cae6099c504de75fefccb78fdec35d283094b1f5
Vigneshwaran123 <51016246+Vigneshwaran123@users.noreply.github.com>
2019-05-31 04:59:40 +0530

    Create alpha.py

---
+++ alpha.py
@@ -0,0 +1,5 @@
+name=input()
+if(name=='a'or name=='b'or name=='c'or name=='d' or name=='e' or name=='f' or name=='g' or name=='h' or name=='i' or name=='j' or name=='k' or name=='l' or name=='m' or name=='n' or name=='o' or name=='p' or name=='q' or name=='r' or name=='s' or name=='t' or name=='u' or name=='v' or name=='w' or name=='x' or name=='y' or name=='z' or name=='A' or name=='B' or name=='C' or name=='D' or name=='E' or name=='F' or name=='G' or name=='H' or name=='I' or name=='J' or name=='K' or name=='L' or name=='M' or name=='N' or name=='O' or name=='P' or name=='Q' or name=='R' or name=='S' or name=='T' or name=='U' or name=='V' or name=='W' or name=='X' or name=='Y' or name=='Z'):
+  print("Alphabet")
+else:
+  print("No")
